d909b91e799c410d9245b2ed7cecee00ebe109f1
clstanton <cstanton77@gmail.com>
2020-04-04 11:20:54 -0500

    while loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,24 +10,43 @@ var enemyAttack = 12;
 // create function
 var fight = function(enemyName) {
     // repeat and execute as long as the enemy robot is alive
-    while(enemyHealth > 0) {
+    while(enemyHealth > 0 && playerHealth > 0) {
         // Fight or skip?
         var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
-    
-        // if player choses to fight, then fight
-        if (promptFight === "fight" || promptFight === "FIGHT") {
+        
+        // if player chooses to skip, confirm and stop the loop
+        if (promptFight === "skip" || promptFight === "SKIP") {
+            // confirm user wants to skip
+            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+            //if yes (true), leave fight
+            if (confirmSkip) {
+                window.alert(playerName + " has decided to skip this fight. Goodbye!");
+                // subtract money from playerMoney for skipping
+                playerMoney = playerMoney - 10;
+                console.log("playerMoney", playerMoney);
+                break;
+            }
+        }
+        // if player chooses to fight, then fight
+        // if (promptFight === "fight" || promptFight === "FIGHT") {
 
             // remove enemy's health by subtracting the amount set in the playerAttack variable
             enemyHealth = enemyHealth - playerAttack;
-            // Log a resulting message to the console so we know that it worked.
+            // log a resulting message to the console so we know that it worked.
             console.log(
                 playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
             );
-            // Check enemy's health
+            // check enemy's health
             if (enemyHealth <= 0) {
                 window.alert(enemyName + " has died!");
-            }
-            else {
+
+                //award player money for winning
+                playerMoney = playerMoney = 20;
+
+                // leave while() loop since enemy is dead.
+                break;
+            } else {
                 window.alert(enemyName + " still has " + enemyHealth + " health left.");
             }
 
@@ -37,34 +56,27 @@ var fight = function(enemyName) {
             console.log(
                 enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
             );
+
             // check player's health
             if (playerHealth <= 0) {
                 window.alert(playerName + " has died!");
+                break;
             } else {
                 window.alert(playerName + " still has " + playerHealth + " health left.");
             }
-
-        // if player chooses to skip
-        } else if (promptFight === "skip" || promptFight === "SKIP") {
-            // confirm user wants to skip
-            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-            //if yes (true), leave fight
-            if (confirmSkip) {
-                window.alert(playerName + " has decided to skip this fight. Goodbye!");
-                // subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 2;
-            }
-            // if no (false), ask question again by running fight() again
-            else {
-                fight();
-            }
-
-        } else {
-            window.alert("You need to pick a valid option. Try again!");
         }
-    }
-};
+    };
+
+            // if no (false), ask question again by running fight() again
+            // else {
+                //fight();
+            //}
+
+        //} else {
+            //window.alert("You need to pick a valid option. Try again!");
+        //}
+    //}
+//};
 
 // fight () function call
 for (var i = 0; i < enemyNames.length; i++) {
